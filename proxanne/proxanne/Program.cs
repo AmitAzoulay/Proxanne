0f867ec8ce6729989a33547e964243eb792185ba
AmitAzoulay <amitazoulay1611@gmail.com>
2025-08-21 15:24:41 +0300

    Added full requirements

--- proxanne/proxanne/Program.cs
+++ proxanne/proxanne/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
 using System.Net;
+using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
 
 
 public class proxanne
 {
     class TCPProxy
     {
-
-        public async Task start(IPEndPoint proxyEndPoint, IPEndPoint remoteEndPoint)
+        public async Task start(IPEndPoint proxyEndPoint)
         {
             TcpListener proxyListener = new TcpListener(IPAddress.Any, proxyEndPoint.Port);
             proxyListener.Start();
@@ -20,44 +22,117 @@ public class proxanne
                 TcpClient clientSocket = await proxyListener.AcceptTcpClientAsync();
                 Console.WriteLine("Client connected...");
 
-                _ = handleClientAsync(clientSocket, remoteEndPoint);
-                
-               
+                _ = handleClientAsync(clientSocket);
+
+
             }
         }
-        public async Task handleClientAsync(TcpClient clientSocket, IPEndPoint remoteEndPoint)
+        public async Task handleClientAsync(TcpClient clientSocket)
         {
             TcpClient remoteServer = new TcpClient();
+            IPEndPoint remoteEndPoint;
+            IPAddress[] addresses;
+            StreamWriter writer;
+            NetworkStream clientStream, remoteStream;
+            StreamReader reader;
+            StreamWriter remoteWriter;
+            string requestLine = string.Empty, remainingRequest = string.Empty, line = string.Empty, remoteDomainName = string.Empty;
+            Ping pingToServer;
+            PingReply replyFromServer;
+            byte[] requestBytes;
+            Task clientToServer, serverToClient;
+
             using (clientSocket)
             {
                 using (remoteServer)
                 {
-                    try
+                    using (writer = new StreamWriter("TCPProxy.log", append: true))
                     {
-                        remoteServer.ConnectAsync(remoteEndPoint);
-                        Console.WriteLine("Proxy connected to" + remoteEndPoint);
+                        try
+                        {
+                            clientStream = clientSocket.GetStream();
+                            reader = new StreamReader(clientStream);
+                           
+                            requestLine = await reader.ReadLineAsync();
+                            remainingRequest = requestLine + "\r\n";
 
-                        NetworkStream clientStream = clientSocket.GetStream();
-                        NetworkStream remoteStream = remoteServer.GetStream();
-                        
-                        Task clientToServer = clientStream.CopyToAsync(remoteStream);
-                        Task serverToClient = remoteStream.CopyToAsync(clientStream);
+                            while (!string.IsNullOrEmpty(line = await reader.ReadLineAsync()))
+                            {
+                                remainingRequest += line + "\r\n";
+                            }
 
-                        await Task.WhenAny(clientToServer, serverToClient); 
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine("Error handling client" + ex.ToString());
+                            remainingRequest += "\r\n";
+                            remoteDomainName = remainingRequest.Split("Host: ")[1];
+
+                            if (remoteDomainName.Contains(":"))
+                            {
+                                remoteDomainName = remoteDomainName.Split(":")[0];
+                            }
+                            
+                            writer.WriteLine("\n\nHost: " + remoteDomainName);
+                            
+                            if (remoteDomainName.Contains("\r") || remoteDomainName.Contains("\n"))
+                            {
+                                remoteDomainName = remoteDomainName.Replace("\r", "").Replace("\n", "");
+                            }
+                            
+                            addresses = Dns.GetHostAddresses(remoteDomainName);
+
+                            if (remainingRequest.Contains("www.google.com"))
+                            {
+                                remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+                                writer.WriteLine("Google catched : forward to localhost:8000");
+                            }
+                            else
+                            {
+                                remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7000);
+                            }
+
+                            pingToServer = new Ping();
+                            writer.WriteLine("Ping to: " + addresses[0]);
+                            replyFromServer = pingToServer.Send(addresses[0]);
+
+                            if (replyFromServer.Status == IPStatus.Success)
+                            {
+                                if (!(replyFromServer.Options.Ttl > 128 || replyFromServer.Options.Ttl < 64))
+                                {
+                                    writer.WriteLine(remoteDomainName + " is a windows server");
+                                    writer.WriteLine("Proxy close the session");
+                                    return;
+                                }
+                                else
+                                {
+                                    writer.WriteLine(remoteDomainName + " is a Non-windows server");
+                                }
+                            }
+                            
+                            await remoteServer.ConnectAsync(remoteEndPoint);
+                            Console.WriteLine("Proxy connected to " + remoteEndPoint);
+
+                            remoteStream = remoteServer.GetStream();
+                            remoteWriter = new StreamWriter(remoteStream);
+
+                            requestBytes = System.Text.Encoding.ASCII.GetBytes(remainingRequest);
+                            await remoteStream.WriteAsync(requestBytes, 0, requestBytes.Length);
+
+                            clientToServer = clientStream.CopyToAsync(remoteStream);
+                            serverToClient = remoteStream.CopyToAsync(clientStream);
+
+                            await Task.WhenAny(clientToServer, serverToClient);
+                        }
+                        catch (Exception ex)
+                        {
+                            writer.WriteLine("Error handling client" + ex.ToString());
+                        }
                     }
                 }
             }
         }
     }
-    
+
     public static async Task Main(String[] args)
     {
         TCPProxy Proxy = new TCPProxy();
-        await Proxy.start(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000), new IPEndPoint(IPAddress.Parse("127.0.0.1"), 7000));
-
+        await Proxy.start(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9000));
     }
 }
